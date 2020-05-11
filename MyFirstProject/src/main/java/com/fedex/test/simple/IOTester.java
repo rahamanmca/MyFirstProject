db11a936205544b202e1478c3bf9ecae097803dd
abdulbigdata <abdul.bigdata@gmail.com>
2020-05-11 21:24:08 +0530

    addned new io

--- MyFirstProject/src/main/java/com/fedex/test/simple/IOTester.java
+++ MyFirstProject/src/main/java/com/fedex/test/simple/IOTester.java
@@ -7,6 +7,7 @@ import org.apache.commons.io.FileSystemUtils;
 public class IOTester {
    public static void main(String[] args) {
       try {
+    	  System.out.println("abdul");
          System.out.println("Free Space " + FileSystemUtils.freeSpaceKb("C:/") + " Bytes");
       } catch(IOException e) {
          System.out.println(e.getMessage());
