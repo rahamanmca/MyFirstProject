ebe4697460e56c79ddea841506235728efa0373a
abdulbigdata <abdul.bigdata@gmail.com>
2020-05-11 21:16:47 +0530

    initial commit

---
+++ MyFirstProject/src/main/java/com/fedex/test/simple/IOTester.java
@@ -0,0 +1,17 @@
+package com.fedex.test.simple;
+
+import java.io.IOException;
+
+import org.apache.commons.io.FileSystemUtils;
+
+public class IOTester {
+   public static void main(String[] args) {
+      try {
+         System.out.println("Free Space " + FileSystemUtils.freeSpaceKb("C:/") + " Bytes");
+      } catch(IOException e) {
+         System.out.println(e.getMessage());
+      }
+   }
+}
+
+
